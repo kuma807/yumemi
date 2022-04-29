fec142c028184adec16c4b720baa395d48bfb796
kuma807 <kumakura013@gmail.com>
2022-04-30 03:37:05 +0900

    add Prefectures

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React, { useState } from "react";
 import Header from "./components/Header";
+import Prefectures from "./components/Prefectures";
 
 const App = () => {
+  const [prefectureSelected, setSelected] = useState(
+    Array.from({ length: 47 }, () => false)
+  );
   return (
     <div>
       <Header />
+      <body style={{ paddingTop: 100 }}>
+        <Prefectures
+          prefectureSelected={prefectureSelected}
+          setSelected={(selectedData) => setSelected(selectedData)}
+        />
+      </body>
     </div>
   );
 };
